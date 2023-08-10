f23e103196c55882e5f64bb13df51642450b4d73
sakshi076 <92707910+sakshi076@users.noreply.github.com>
2023-08-10 14:34:00 +0530

    Add files via upload

---
+++ non-repeating.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string s="programming";
+	int n=s.length();
+	int flag=0;
+		for(int i=0;i<n;i++){
+		
+		for(int j=i+1;j<n;j++)
+		{
+			if(s[i]!=s[j])
+			{
+				flag=1;
+			}
+			else
+			{
+				flag=0;
+				break;
+			}	
+		}
+		if(flag==1){
+			cout<<s[i];
+			break;
+		}	
+	}
+	
+	
+}
